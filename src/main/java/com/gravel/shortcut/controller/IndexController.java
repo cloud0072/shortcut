b68d73dcb3f1f69aecc2b67aa3350f7e7738d439
caolei <352419394@qq.com>
2021-07-29 10:30:44 +0800

    fix up

--- src/main/java/com/gravel/shortcut/controller/IndexController.java
+++ src/main/java/com/gravel/shortcut/controller/IndexController.java
@@ -25,6 +25,11 @@ public class IndexController {
     @Value("${common.domain}")
     private String domain;
 
+    @GetMapping(value = "/")
+    public String home() {
+        return "redirect:https://www.yunbenz.com";
+    }
+
     @GetMapping(value = "/index")
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView("index");
